df08d2b2010758b2a070f3eb6c9b284310d74150
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2022-05-30 07:53:44 +0200

    iproxy fetcher

---
+++ src/hexData.ts
@@ -0,0 +1,72 @@
+export class HexData {
+    private static readonly VALID_CHARS: Record<string, true | undefined> = {
+        "0": true,
+        "1": true,
+        "2": true,
+        "3": true,
+        "4": true,
+        "5": true,
+        "6": true,
+        "7": true,
+        "8": true,
+        "9": true,
+        a: true,
+        b: true,
+        c: true,
+        d: true,
+        e: true,
+        f: true,
+        A: true,
+        B: true,
+        C: true,
+        D: true,
+        E: true,
+        F: true,
+    };
+    public hexString: string;
+    constructor(hexString: string) {
+        if (!this.verifyHexString(hexString)) {
+            throw new Error("Invalid hex string");
+        }
+        this.hexString = hexString.substring(2);
+    }
+    getNumber(): number {
+        return parseInt(this.hexString, 16);
+    }
+    getAscii(): string {
+        let rv = "";
+        for (let i = 0; i < this.hexString.length; i += 2) {
+            const charCode = parseInt(this.hexString.substring(i, i + 2), 16);
+            rv += String.fromCharCode(charCode);
+        }
+        return rv;
+    }
+    getHexString(): string {
+        return "0x" + this.hexString;
+    }
+    verifyHexString(hexString: string): boolean {
+        return hexString.startsWith("0x") && this.verifyChars(hexString.substring(2));
+    }
+
+    static fromString(str: string): HexData {
+        str = str.toLocaleLowerCase();
+        if (str.startsWith("0x")) return new HexData(str);
+        return new HexData("0x" + str);
+    }
+    static fromNumber(num: number): HexData {
+        return new HexData(`0x${num.toString(16)}`);
+    }
+    private verifyChars(str: string): boolean {
+        for (let i = 0; i < str.length; i++) {
+            if (!HexData.VALID_CHARS[str[i] as string]) return false;
+        }
+        return true;
+    }
+    static parseHexToNumberUnsafe(hex: string): number {
+        return parseInt(hex.substring(2), 16);
+    }
+    static parseHexToBigIntUnsafe(hex: string): BigInt {
+        if (hex.length % 2) { hex = '0' + hex; }
+        return BigInt(hex);
+    }
+}
---
+++ src/interfaces/IproxyFetcher.ts
@@ -0,0 +1,38 @@
+/* eslint-disable import/no-unresolved */
+
+import { HexData } from "../hexData";
+
+/**
+ * ProxyFetcher is a specialiezed fetcher for Geth/Parity proxy to use with etherscan.  
+ * ProxyFetcher maps 1 to 1 with the API methods provieded by etherscan.  
+ * This fetcher implements the Geth/Parity proxy methods. Other methods are implemented in other fetchers.  
+ * Other fetchers are: ProFetcher for [Pro API methods](https://docs.etherscan.io/api-pro/api-pro) and Fetcher for standard methods.
+ */
+export interface IProxyFetcher {
+    getBlockNumber(): Promise<number>;
+    getBlock(blockNumber: number): Promise<Block>;
+}
+
+export interface Block {
+    baseFeePerGas: BigInt;
+    difficulty: BigInt;
+    extraData: HexData;
+    gasLimit: number;
+    gasUsed: number;
+    hash: string;
+    logsBloom: HexData;
+    miner: string;
+    mixHash: string;
+    nonce: string;
+    number: number;
+    parentHash: string;
+    recieptRoot: string;
+    sha3Uncles: string;
+    size: number;
+    stateRoot: string;
+    timestamp: Date;
+    totalDifficulty: BigInt;
+    transactions: Array<string>;
+    transactionsRoot: string;
+    uncles: Array<string>;
+}
